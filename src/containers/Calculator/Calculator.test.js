e2915b8a3aea3ab93b24b5b7759dadeef4c01abe
Sergejs Basangovs <sergejs.basangovs@ingresso.co.uk>
2020-07-29 10:59:40 +0300

    testing libraries and tests updated

--- src/containers/Calculator/Calculator.test.js
+++ src/containers/Calculator/Calculator.test.js
@@ -1,12 +1,12 @@
 import { shallow } from "enzyme";
 import React from "react";
 
-import CalculatorApp from "./CalculatorApp.component";
+import Calculator from "./Calculator.component";
 
-describe("CalculatorApp", () => {
+describe("Calculator", () => {
 	let wrapper;
 	beforeEach(() => {
-		wrapper = shallow(<CalculatorApp />);
+		wrapper = shallow(<Calculator />);
 	});
 	it("should render the component", () => {
 		expect(wrapper.length).toEqual(1);
